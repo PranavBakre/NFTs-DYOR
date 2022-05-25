7aac0429e668a8c748ae73877c1988613e9a153c
Pranav Bakre <pranavbakre@yahoo.in>
2022-05-26 00:58:37 +0530

    feat: Base setup for the scene
    Added extremely rudimentary configuration for renderer, camera and camera controls

---
+++ src/configuration/setup.js
@@ -0,0 +1,15 @@
+import { Vector3 } from 'three';
+import { Fog } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+export const renderer = new WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+export const scene = new Scene();
+export const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+export const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.target = new Vector3(0,1.4,0)
+controls.autoRotate = true
